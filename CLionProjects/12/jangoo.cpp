4a6b965b3ba4ddeba6b0b7f2d9c93502edbf796f
Jangoo32 <58041979+jangoo32@users.noreply.github.com>
2020-11-09 17:42:29 +0200

    Update jangoo.cpp

--- CLionProjects/12/jangoo.cpp
+++ CLionProjects/12/jangoo.cpp
@@ -1,50 +1,47 @@
-
+/*mohammed adam
+secend class
+computer*/
 #include <iostream>
 #include <string>
 using namespace std;
-int main()
-{
-    string name, section;
-    int username, grade;
-    cout << "Enter your name= ";
-    cin >> name;
-    cout << "Enter your usrename= ";
-    cin >> username;
-    cout << "Enter your section A or T or C= ";
-    cin >> section;
-    cout << "Enter your grade = ";
-    cin >> grade;
-    cout << "Name: " << name;
-    cout << "Username: " << username ;
-    if (section == "A")
-    {
-        cout << "section: Control";
-    }
-    if (section == "T"){
-        cout << "section: Communication";
-    }
-    if (section == "C"){
-        cout << "section: Computer";
-    }
-    switch (grade){
-    case 0 ... 59:
-        cout << "grade: F";
-        break;
-    case 60 ... 69:
-        cout << "grade: D";
-        break;
-    case 70 ... 79:
-        cout << "grade: C";
-        break;
-    case 80 ... 89:
-        cout << "grade: B";
-        break;
-    case 90 ... 100:
-        cout << "grade: A";
-        break;
-    default:
-        cout << "error";
-    }
-
+int main(){
+    string student, section;
+    int id, grade;
+        cout << "Enter the student name= ";
+        cin >> student;
+        cout << "Enter the student id= ";
+        cin >> id;
+        cout << "Enter the student section A or T or C= ";
+        cin >> section;
+        cout << "Enter the student grade= ";
+        cin >> grade;
+        cout << "the student name  = " << student << endl;
+        cout << "id= " << id << endl;
+        if (section == "A"){
+            cout << "section = Control" << endl;}
+        else
+            if (section == "T"){
+            cout << "section = Communication" << endl;}
+            else if (section == "C"){
+            cout << "section = Computer" << endl;}
+        switch (grade){
+            case 90 ... 100:
+                cout << "grade= A" << endl;
+                break;
+            case 80 ... 89:
+                cout << "grade= B" << endl;
+                break;
+            case 70 ... 79:
+                cout << "grade= C" << endl;
+                break;
+            case 60 ... 69:
+                cout << "grade= D" << endl;
+                break;
+            case 0 ... 59:
+                cout << "grade= F" << endl;
+                break;
+            default:
+                cout << "error" << endl;
+        }
     return 0;
 }
